7d6baa6da54a7a79c94f3e2c8d8f51e3a684e8b1
Camed23 <faridabouabakari513@gmail.com>
2025-05-01 23:26:17 +0200

    new version deploy

--- server.cjs
+++ server.cjs
@@ -26,12 +26,15 @@ app.get('/', (req, res) => {
 });
 
 
+require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'toeic800',
-    database: 'users_db',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306
+
 });
 
 
